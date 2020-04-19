9aa912dfad7353bd484e4efba6d20def345a3e32
Alex Khoa Le <leduykhoa@hotmail.ca>
2020-04-19 18:02:09 -0400

    Properly spaced nav items

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,14 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import styled from 'styled-components';
 import { useStaticQuery, graphql, Link } from 'gatsby';
 import Img from 'gatsby-image';
-// import Navbar from 'react-bootstrap/Navbar';
-// import Nav from 'react-bootstrap/Nav';
-
-const Nav = styled.nav`
-display: flex;
-justify-content: space-between;
-`
 
 const logo = graphql`
 query {
@@ -62,39 +54,14 @@ const Navmenu = () => {
                         className="navImg"
                     />
                 </div>
-                <div className="navItems align-bottom" style={{paddingBottom: '20px'}}>
-                    <Nav>
-                        <Link to="/" className="link">Home</Link>
-                        <Link to="/about" className="link">About Moringa</Link>
-                        <Link to="/grow-moringa" className="link">Grow Organic Moringa</Link>
-                        <Link to="/certification" className="link">Get Organic Certification</Link>
-                        <Link to="/partners" className="link">Partners</Link>
-                        <Link to="/contact" className="link">Contact</Link>
-                    </Nav>
-                </div>
+                <Link to="/" className="link">Home</Link>
+                <Link to="/about" className="link">About Moringa</Link>
+                <Link to="/grow-moringa" className="link">Grow Organic Moringa</Link>
+                <Link to="/certification" className="link">Get Organic Certification</Link>
+                <Link to="/partners" className="link">Partners</Link>
+                <Link to="/contact" className="link">Contact</Link>
             </div></div>
         </div>
-
-    // return (
-    //     <Navbar bg="light" className="navContainer">
-    //         <Navbar.Brand href="/">
-    //             <Img
-    //                 // Image is dynamically selected depending on current window size
-    //                 imgStyle={{ objectFit: 'contain' }}
-    //                 // style={{ height: windowSize.desktop ? data.desktop.childImageSharp.fixed.presentationHeight : data.mobile.childImageSharp.fixed.presentationHeight }}
-    //                 fixed={windowSize.desktop ? data.desktop.childImageSharp.fixed : data.mobile.childImageSharp.fixed}
-    //                 alt="primary logo"
-    //             />
-    //         </Navbar.Brand>
-    //         <Nav className="mr-auto">
-    //             <Nav.Link href="/" className="link">Home</Nav.Link>
-    //             <Nav.Link href="/about" className="link">About Moringa</Nav.Link>
-    //             <Nav.Link href="/grow-moringa" className="link">Grow Organic Moringa</Nav.Link>
-    //             <Nav.Link href="/certification" className="link">Get Organic Certification</Nav.Link>
-    //             <Nav.Link href="/partners" className="link">Partners</Nav.Link>
-    //             <Nav.Link href="/contact" className="link">Contact</Nav.Link>
-    //         </Nav>
-    //     </Navbar>
     );
 }
 
