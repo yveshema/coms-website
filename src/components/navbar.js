55db06e59766c8a8abb7eaa9e1de04b0407ef325
Alex Khoa Le <leduykhoa@hotmail.ca>
2020-04-20 19:27:40 -0400

    Added close icon, mobile nav state, function to toggle mobile nav, and divs for mobile nav styling

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -28,6 +28,14 @@ query {
             }
         }
     }
+    closeNavIcon: file(relativePath: { eq: "icons/ui-icons/close32.png" })
+    {
+        childImageSharp {
+            fixed (height: 48) {
+                ...GatsbyImageSharpFixed
+            }
+        }
+    }
     hamburger: file(relativePath: { eq: "icons/ui-icons/menu-48.png" })
     {
         childImageSharp {
@@ -48,15 +56,17 @@ query {
 `
 
 const Navmenu = () => {
-    const [windowSize, changeWindowSize] = useState({ 
+    const [windowSize, changeWindowSize] = useState({
         desktop: window.matchMedia("(min-width: 768px)").matches,
-        navBurger: window.matchMedia("(min-width: 1280px)").matches
+        navBurger: window.matchMedia("(min-width: 1280px)").matches,
+        mobileNavActive: false
     });
 
     // Checks if the current window size is desktop or mobile
     useEffect(() => {
         function handleWindowChange() {
             changeWindowSize({
+                ...windowSize,
                 desktop: window.matchMedia("(min-width: 768px)").matches,
                 navBurger: window.matchMedia("(min-width: 1280px)").matches
             });
@@ -68,35 +78,49 @@ const Navmenu = () => {
     }
     )
 
+    const changeNavState = () => {
+        changeWindowSize({
+            ...windowSize,
+            mobileNavActive: !windowSize.mobileNavActive
+        })
+    }
+
     const data = useStaticQuery(logo);
 
     return (
         <div className="navContainer">
-        <div className="navWrapper">
-            <div className="row" style={{height: '100%'}}>
-                <div style={{width:"170px"}}>
-                    <Img
-                        // Image is dynamically selected depending on current window size
-                        fixed={windowSize.desktop ? data.desktop.childImageSharp.fixed : data.mobile.childImageSharp.fixed}
-                        alt="primary logo"
-                        className="navImg"
-                    />
-                </div>
-                <Nav style={!windowSize.navBurger ? {justifyContent: "flex-end"} : {}}>
-                    <Link to="/" className={windowSize.navBurger ? "link" : "link mobile"} activeClassName="linkActive">Home</Link>
-                    <Link to="/about" className="link" activeClassName="linkActive">About Moringa</Link>
-                    <Link to="/cultivation" className="link" activeClassName="linkActive">Grow Organic Moringa</Link>
-                    <Link to="/certification" className="link" activeClassName="linkActive">Get Organic Certification</Link>
-                    <Link to="/partners" className="link" activeClassName="linkActive">Partners</Link>
-                    <Link to="/contact" className="link" activeClassName="linkActive">Contact</Link>
-                    <button className="hamburgBtn">
+            <div className="navWrapper">
+                <div className="row" style={{ height: '100%' }}>
+                    <div style={{ width: "170px" }}>
+                        <Img
+                            // Image is dynamically selected depending on current window size
+                            fixed={windowSize.desktop ? data.desktop.childImageSharp.fixed : data.mobile.childImageSharp.fixed}
+                            alt="primary logo"
+                            className="navImg"
+                        />
+                    </div>
+                    <Nav className={!windowSize.navBurger ? windowSize.mobileNavActive ? `navMobile` : `navHidden` : {}}>
+                        <Link to="/" className={windowSize.navBurger ? "link" : "link mobile"} activeClassName="linkActive">Home</Link>
+                        <Link to="/about" className={windowSize.navBurger ? "link" : "link mobile"} activeClassName="linkActive">About Moringa</Link>
+                        <Link to="/cultivation" className={windowSize.navBurger ? "link" : "link mobile"} activeClassName="linkActive">Grow Organic Moringa</Link>
+                        <Link to="/certification" className={windowSize.navBurger ? "link" : "link mobile"} activeClassName="linkActive">Get Organic Certification</Link>
+                        <Link to="/partners" className={windowSize.navBurger ? "link" : "link mobile"} activeClassName="linkActive">Partners</Link>
+                        <Link to="/contact" className={windowSize.navBurger ? "link" : "link mobile"} activeClassName="linkActive">Contact</Link>
+                        <div className="mobileNavBg">
+                            <button className="closeNavBtn" onClick={changeNavState}>
+
+                            </button>
+                        </div>
+                        <div className="mobileNavOverlay" onClick={changeNavState}>
+                        </div>
+                    </Nav>
+                    <button className="hamburgBtn" onClick={changeNavState}>
                         <Img
                             fixed={windowSize.desktop ? data.hamburger.childImageSharp.fixed : data.hamburgerMobile.childImageSharp.fixed}
                             alt="hamburger"
                         />
                     </button>
-                </Nav>
-            </div></div>
+                </div></div>
         </div>
     );
 }
