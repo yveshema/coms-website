960f9e1974a6c8ab7e89f0ca26721e2d05b179d6
Alex Khoa Le <leduykhoa@hotmail.ca>
2020-04-20 02:54:39 -0400

    Reverted styled div Nav, added hamburger icons, added hamburger button for tablet and mobile

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,6 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import { useStaticQuery, graphql, Link } from 'gatsby';
 import Img from 'gatsby-image';
+import styled from "styled-components"
+
+const Nav = styled.div`
+    display: flex;
+    justify-content: space-between;
+    width: 100%;
+    margin-left: 10px;
+`
 
 const logo = graphql`
 query {
@@ -20,17 +28,37 @@ query {
             }
         }
     }
+    hamburger: file(relativePath: { eq: "icons/ui-icons/menu-48.png" })
+    {
+        childImageSharp {
+            fixed (height: 48) {
+                ...GatsbyImageSharpFixed
+            }
+        }
+    }
+    hamburgerMobile: file(relativePath: { eq: "icons/ui-icons/menu-48.png" })
+    {
+        childImageSharp {
+            fixed (height: 32) {
+                ...GatsbyImageSharpFixed
+            }
+        }
+    }
 }
 `
 
 const Navmenu = () => {
-    const [windowSize, changeWindowSize] = useState({ desktop: window.matchMedia("(min-width: 768px)").matches });
+    const [windowSize, changeWindowSize] = useState({ 
+        desktop: window.matchMedia("(min-width: 768px)").matches,
+        navBurger: window.matchMedia("(min-width: 1280px)").matches
+    });
 
     // Checks if the current window size is desktop or mobile
     useEffect(() => {
         function handleWindowChange() {
             changeWindowSize({
-                desktop: window.matchMedia("(min-width: 768px)").matches
+                desktop: window.matchMedia("(min-width: 768px)").matches,
+                navBurger: window.matchMedia("(min-width: 1280px)").matches
             });
         }
         window.addEventListener('resize', handleWindowChange);
@@ -54,12 +82,20 @@ const Navmenu = () => {
                         className="navImg"
                     />
                 </div>
-                <Link to="/" className="link">Home</Link>
-                <Link to="/about" className="link">About Moringa</Link>
-                <Link to="/grow-moringa" className="link">Grow Organic Moringa</Link>
-                <Link to="/certification" className="link">Get Organic Certification</Link>
-                <Link to="/partners" className="link">Partners</Link>
-                <Link to="/contact" className="link">Contact</Link>
+                <Nav style={!windowSize.navBurger ? {justifyContent: "flex-end"} : {}}>
+                    <Link to="/" className={windowSize.navBurger ? "link" : "link mobile"}>Home</Link>
+                    <Link to="/about" className="link">About Moringa</Link>
+                    <Link to="/grow-moringa" className="link">Grow Organic Moringa</Link>
+                    <Link to="/certification" className="link">Get Organic Certification</Link>
+                    <Link to="/partners" className="link">Partners</Link>
+                    <Link to="/contact" className="link">Contact</Link>
+                    <button className="hamburgBtn">
+                        <Img
+                            fixed={windowSize.desktop ? data.hamburger.childImageSharp.fixed : data.hamburgerMobile.childImageSharp.fixed}
+                            alt="hamburger"
+                        />
+                    </button>
+                </Nav>
             </div></div>
         </div>
     );
