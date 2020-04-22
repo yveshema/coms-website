f5a41e785d868854612cbcf993ccc00068c2722d
Alex Khoa Le <leduykhoa@hotmail.ca>
2020-04-22 19:58:43 -0400

    Added functions to toggle search bar and language options. Added reactive functionality to components

--- src/components/search-bar.js
+++ src/components/search-bar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useStaticQuery, graphql } from 'gatsby';
 import Img from 'gatsby-image';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -14,20 +14,53 @@ query {
             }
         }
     }
+    check: file(relativePath: { eq: "icons/ui-icons/checkmark-48.png" })
+    {
+        childImageSharp {
+            fixed (height: 20) {
+                ...GatsbyImageSharpFixed
+            }
+        }
+    }
 }`
 
 const SearchBar = (props) => {
+    const [searchState, changeSearch] = useState({
+        searchBarVisible: props.currWidth,
+        languageDropdownVisible: false
+    })
+
+    const openSearchBar = () => {
+        changeSearch({
+            ...searchState,
+            searchBarVisible: !searchState.searchBarVisible
+        })
+    }
+
+    const openLanguageOptions = () => {
+        changeSearch({
+            ...searchState,
+            languageDropdownVisible: !searchState.languageDropdownVisible
+        })
+    }
+
     const data = useStaticQuery(icon);
     return (
         <div className="searchDiv">
-            <input type="text" id="search" placeholder="Search..." autoComplete="off"/>
-            <button className="searchOpenBtn">
+            <input type="text" id="search" style={searchState.searchBarVisible ? {display: "block"} : {display: "none"}} placeholder="Search..." autoComplete="off"/>
+            <button className="searchOpenBtn" onClick={openSearchBar}>
                 <Img 
                     fixed={data.search.childImageSharp.fixed}
                     alt="Search Icon"
                 />
             </button>
-            <button className="languageBtn">ENG <FontAwesomeIcon icon={faChevronDown} /></button>
+            <button className="languageBtn" onClick={openLanguageOptions}>
+                ENG <FontAwesomeIcon icon={searchState.languageDropdownVisible ? faChevronUp : faChevronDown} />
+            </button>
+            <div className="dropdownSubMenu languageOptionDiv" style={searchState.languageDropdownVisible ? {display: "block", maxWidth: "100px", top: "30px"} : {display: "none"}}>
+                <button className="languageOptionBtn">English</button>
+                <button className="languageOptionBtn">Fran&ccedil;ais</button>
+            </div>
         </div>
     )
 }
