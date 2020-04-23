75169761267a38ceb6ec9b7e4dd71d1dd850b0a0
Alex Khoa Le <leduykhoa@hotmail.ca>
2020-04-23 19:53:11 -0400

    Added breakpoints to modify separator style

--- src/components/separator.js
+++ src/components/separator.js
@@ -2,8 +2,24 @@ import React from 'react';
 import styled from 'styled-components';
 
 const Wrapper = styled.span`
-  color: white;
-  font-size: 1em;
+  border-left: 1px solid white;
+  height: auto;
+  user-select: none;
+  color: transparent;
+
+  @media only screen and (max-width: 1280px) {
+    :last-of-type {
+      order: 4;
+      flex-basis: 100%;
+    }
+    height: 0;
+  }
+  @media only screen and (max-width: 675px) {
+    :first-of-type {
+      order: 2;
+      flex-basis: 100%;
+    }
+  }
 `;
 
 const Separator = () => (
