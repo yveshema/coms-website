279087e1dff97431bdd3c6aec15872f07c25af89
Yves Shema <admin@rysapp.com>
2020-04-23 16:39:07 -0700

    Configure AWS authentication
    Authentication configured only to enable login. At this stage, this is deemed sufficient for the purposes of restricting access to the site. Once logged in, navigation of the site is possible but mechanism for logging out is provided at the moment as it is not at the moment clear what value such functionality would add.

--- src/components/guardedRoute.js
+++ src/components/guardedRoute.js
@@ -1,10 +1,10 @@
 import React, { useEffect, useState } from "react";
-import { navigate } from "@reach/router";
-import { isLoggedIn, handleLogin } from "../services/auth";
+// import { navigate } from "@reach/router";
 import styled from "styled-components";
 import { Helmet } from "react-helmet";
+import { Auth } from "aws-amplify";
 
-
+// This positions the login form in the middle of the page.
 const LoginContainer = styled.div`
 height: 100vh;
 display: flex;
@@ -13,67 +13,86 @@ justify-content: space-evenly;
 align-items: center;
 `;
 
+// Wrap the root element in authentication logic so 
+// to restricted access to the site to only authorized people.
+// Only required during development to allow testing in a live environment.
+// Because this wraps the root element, no session management is required to
+// persist state. 
 
 const GuardedRoute = ({children}) => {
-    
+
     const [credentials, setCredentials] = useState({
         username: '',
         password: '',
     });
 
-    const [done, setDone] = useState(false);
+    const [done, setDone] = useState(false); 
+    
+    const [ loggedIn, setLoggedIn ] = useState(false);
 
-    useEffect(() => {
-        if (isLoggedIn()){
-            navigate("/");
-        }
-    }, [done]);
+    // useEffect(() => {
+    //     if (loggedIn){
+    //         navigate("/");
+    //     }
+    // }, [done,loggedIn]);
     
     const handleUpdate = event => {
         setCredentials({...credentials, [event.target.name]:event.target.value});
     }
 
-    const handleSubmit = event => {
+    const handleSubmit = async event => {
         event.preventDefault();
-        handleLogin(credentials); 
-        setDone(true);       
+
+        try {
+            await Auth.signIn(credentials.username, credentials.password);
+            setLoggedIn(true);
+            setDone(true);
+        } catch (e) {
+            alert(e.message);
+        }             
     }
 
         
-    if (!isLoggedIn()){
+    if (!loggedIn){
         return (
             <LoginContainer>
-        <Helmet>
-            <title>Login</title>
-        </Helmet>
-        <div id="login"><h1>Log in</h1>
-        <form id="loginForm"
-            method="post"
-            onSubmit={event => {
-                handleSubmit(event);                
-            }}
-        >   
-            <table>         
-            <tr><td><label>Username: </label></td>
-                <td><input type="text" name="username" onChange={handleUpdate} /></td>
-            </tr>
-            <tr><td><label>Password: </label></td> 
-                <td><input type="password" name="password" onChange={handleUpdate} /></td>
-           </tr>
-            <tr><td colspan="2"><input type="submit" value="Log in" /></td></tr>                 
-           </table> 
-        </form>
-        
-        </div>
-        </LoginContainer>
+                <Helmet>
+                    <title>Login</title>
+                </Helmet>
+                <div id="login">
+                    <h1>Log in</h1>
+                    <form id="loginForm"
+                        method="post"
+                        onSubmit={event => {
+                            handleSubmit(event);                
+                        }}
+                    >   
+                        <table>         
+                            <tr><td><label>Username: </label></td>
+                                <td><input type="text" name="username" 
+                                        onChange={handleUpdate} /></td>
+                            </tr>
+                            <tr>
+                                <td><label>Password: </label></td> 
+                                <td><input type="password" name="password" 
+                                    onChange={handleUpdate} /></td>
+                            </tr>
+                            <tr>
+                                <td colspan="2"><input type="submit" value="Log in" /></td>
+                            </tr>                 
+                        </table> 
+                    </form>        
+                </div>
+            </LoginContainer>
         );
     }
 
-    return <>{children}</>;    
+    return <>{children}</>
+        
 }
 
-export default ({element}) => (
+export default ({element}) => (    
     <GuardedRoute>
         {element}
-    </GuardedRoute>
+    </GuardedRoute>    
 );
---
+++ src/config.js
@@ -0,0 +1,7 @@
+export default {
+    cognito: {
+        REGION: "us-west-2",
+        USER_POOL_ID: "us-west-2_afJcbJGVC",
+        APP_CLIENT_ID: "5br7prs8anuq266o316anai2ep"
+    }
+}
