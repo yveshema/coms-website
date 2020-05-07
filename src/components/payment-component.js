f4fa1bbc4777711c50a95bd504cc6cbb0ccef593
Alex Khoa Le <leduykhoa@hotmail.ca>
2020-05-07 14:28:33 -0700

    Payment details are now written to payment component's state

--- src/components/payment-component.js
+++ src/components/payment-component.js
@@ -1,13 +1,37 @@
 import React, { useState } from 'react';
+import ItemSelectionForm from "../components/item-selection";
 import PaymentInfo from './payment-info';
 
 const PaymentComponent = () => {
     const [ formStates, changeForm ] = useState({
-        totalCost: 0
+        totalCost: 0,
+        fullName: '',
+        email: '',
+        cardType: '',
+        lastFour: 0,
+        expiryDate: ''
     })
 
+    const handleCardInfo = payload => {
+        if (payload.paymentMethod === undefined) {
+            console.log("Error:", payload);
+            return;
+        }
+        changeForm({
+            ...formStates,
+            fullName: payload.paymentMethod.billing_details.name,
+            email: payload.paymentMethod.billing_details.email,
+            cardType: payload.paymentMethod.card.brand,
+            lastFour: payload.paymentMethod.card.last4,
+            expiryDate: `${payload.paymentMethod.card.exp_month}/${payload.paymentMethod.card.exp_year}`
+        })
+      };
+
     return (
-        <PaymentInfo />
+        <div>
+            <ItemSelectionForm />
+            <PaymentInfo handleCardInfo={handleCardInfo} />
+        </div>
     )
 }
 
