b28c7b69113a941a9c720a2bfb238edf3931ce54
Alex Khoa Le <leduykhoa@hotmail.ca>
2020-05-14 15:38:18 -0700

    Checkboxes now have checkmarks. Submit button text is replaced by spinner while content is being submitted

--- src/components/item-selection.js
+++ src/components/item-selection.js
@@ -1,5 +1,7 @@
 import React from "react";
 import styled from "styled-components";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleNotch } from '@fortawesome/free-solid-svg-icons';
 
 import {
     Input,
@@ -43,10 +45,54 @@ cursor: pointer;
 transition: 0.25s;
 `;
 
-const Control = styled.div`
+const Control = styled.label`
 display: flex;
-// line-height: 1;
 margin: 1em 0;
+cursor: pointer;
+text-align: left;
+font-size: 1em;
+line-height: 1.2;
+align-items: center;
+
+input {
+    position: absolute;
+    opacity: 0;
+    height: 0;
+    width: 0;
+    cursor: pointer;
+}
+
+.checkmark {
+    position: inherit;
+    height: 25px;
+    width: 25px;
+    margin-right: 15px;
+    border: 1px solid #707070;
+    border-radius: 4px;
+}
+
+.checkSvg {
+    fill: none;
+    stroke: white;
+    stroke-width: 2;
+}
+
+svg {
+    visibility: hidden;
+}
+
+:hover input ~ .checkmark{
+    background-color: #cccccc;
+}
+
+input:checked ~ .checkmark {
+    background-color: #626262;
+}
+
+input:checked ~ .checkmark svg {
+    visibility: visible;
+}
+
 `
 
 const Label = styled.div`
@@ -61,74 +107,11 @@ const StyledInput = styled(Input)`
 width: 14em;
 border: 1px solid #C1C1C1;
 border-radius: 0.25rem;
+font-weight: 500;
 padding: 5px 12px;
 background-color: white;
 box-shadow: none;
 `
-
-// const HiddenCheckbox = styled.input.attrs({ type: 'checkbox' })`
-// // Hide checkbox visually but remain accessible to screen readers.
-// // Source: https://medium.com/@colebemis/building-a-checkbox-component-with-react-and-styled-components-8d3aa1d826dd
-// border: 0;
-// clip: rect(0 0 0 0);
-// clippath: inset(50%);
-// height: 1px;
-// margin: -1px;
-// overflow: hidden;
-// padding: 0;
-// position: absolute;
-// white-space: nowrap;
-// width: 1px;
-// `
-// const Icon = styled.svg`
-// fill: none;
-// stroke: white;
-// stroke-width: 3px;
-// outline: 0;
-// `
-
-// const StyledCheckbox = styled.button`
-// display: inline-block;
-// width: 18px;
-// height: 18px;
-// background: ${props => props.checked ? '#555' : '#fff'};
-// border: ${props => props.checked ? '1px solid #555' : '1px solid #a3a3a3'};
-// transition: all 150ms;
-// margin: 0;
-// padding: 0;
-
-// ${HiddenCheckbox}:focus + & {
-//     box-shadow: 0 0 0 3px #eee;   
-// }
-
-// ${Icon} {
-//     visibility: ${props => props.checked ? 'visible' : 'hidden'}   
-// }
-// `
-// const CheckboxWrapper = styled.div`
-// display: inline-block;
-// vertical-align: middle;
-// line-height: .8;
-// `
-
-// const Checkbox = ({ className, ...props}) => {
-//     const [checked, setChecked ] = useState(false);
-
-//     const handleClick = (e) => {
-//         e.preventDefault();
-//         setChecked(!checked);
-//     }
-
-//     return (
-//     <CheckboxWrapper className={className}>
-//         <HiddenCheckbox checked={checked} {...props} />
-//         <StyledCheckbox checked={checked} onClick={handleClick}>
-//             <Icon viewBox="0 0 24 24">
-//                 <polyline points="20 6 9 17 4 12" />
-//             </Icon>
-//         </StyledCheckbox>
-//     </CheckboxWrapper>
-// )};
 
 const ItemSelectionForm = (props) => {
     return (
@@ -138,31 +121,60 @@ const ItemSelectionForm = (props) => {
             </p>
             <form onSubmit={props.submitTotal}>
                 <Control>
-                    <input type="checkbox" value="sys" onChange={props.changeCheckedStatus} />
-                    <Label>NCD 185 system control fee</Label>
+                    <input type="checkbox" onChange={() => props.changeCheckedStatus("sys")} />
+                    <span className="checkmark">
+                        <svg viewBox="0 0 25 25">
+                            <polyline className="checkSvg" points="23,3 8,20 1,12" />
+                        </svg>
+                    </span>
+                    NCD 185 system control fee
                 </Control>
                 <Control>
-                    <input type="checkbox" value="file" onChange={props.changeCheckedStatus} />
-                    <Label>NCD 55 file management fee</Label>
+                    <input type="checkbox" onChange={() => props.changeCheckedStatus("file")} />
+                    <span className="checkmark">
+                        <svg viewBox="0 0 25 25">
+                            <polyline className="checkSvg" points="23,3 8,20 1,12" />
+                        </svg>
+                    </span>
+                    NCD 55 file management fee
                 </Control>
                 <Control>
-                    <input type="checkbox" value="recert" onChange={props.changeCheckedStatus} />
-                    <Label>NCD 700 recertification fee</Label>
+                    <input type="checkbox" onChange={() => props.changeCheckedStatus("recert")} />
+                    <span className="checkmark">
+                        <svg viewBox="0 0 25 25">
+                            <polyline className="checkSvg" points="23,3 8,20 1,12" />
+                        </svg>
+                    </span>
+                    NCD 700 recertification fee
                 </Control>
                 <Control>
-                    <input type="checkbox" value="under2" onChange={props.changeCheckedStatus} />
-                    <Label>NCD 350 for crops below 2 acres</Label>
+                    <input type="checkbox" onChange={() => props.changeCheckedStatus("under2")} />
+                    <span className="checkmark">
+                        <svg viewBox="0 0 25 25">
+                            <polyline className="checkSvg" points="23,3 8,20 1,12" />
+                        </svg>
+                    </span>
+                    NCD 350 for crops below 2 acres
                 </Control>
                 <Control>
-                    <input type="checkbox" value="over2" onChange={props.changeCheckedStatus} />
-                    <Label>NCD 550 for crops above 2 acres</Label>
+                    <input type="checkbox" onChange={() => props.changeCheckedStatus("over2")} />
+                    <span className="checkmark">
+                        <svg viewBox="0 0 25 25">
+                            <polyline className="checkSvg" points="23,3 8,20 1,12" />
+                        </svg>
+                    </span>
+                    NCD 550 for crops above 2 acres
                 </Control>
                 <hr />
                 <Row>
                     <Label>Total amount to be paid: </Label>
                     <StyledInput type="text" value={'NCD ' + props.totalCost} placeholder="NCD" />
                 </Row>
-                <Row><SubmitButton>Payment Information</SubmitButton></Row>
+                {props.clientSecret === 'Error' &&
+                    <div style={{ fontSize: '0.7em', color: 'red' }}>
+                        There was an issue connecting to Stripe services. Please try again.
+                </div>}
+                <Row><SubmitButton>{props.isLoading ? <FontAwesomeIcon className="spinnerAnim" icon={faCircleNotch} /> : 'Payment Information'}</SubmitButton></Row>
             </form>
         </Container>
     )
