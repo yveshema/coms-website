59e02d5d4ff6c10a6aa35a3ff8e76968a48fbb90
Alex Khoa Le <leduykhoa@hotmail.ca>
2020-06-08 17:57:55 -0700

    Added new hero images for different breakpoints. Adjusted hero responsive behaviour

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,14 +1,5 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
-
-const images = {
-    home: require("../images/heros/homepage-hero-100.jpg"),
-    about: require("../images/heros/About-moringa-hero-100.jpg"),
-    cultivation: require("../images/heros/Cultivation-hero-100.jpg"),
-    transportation: require("../images/heros/transportation-hero-100.jpg"),
-    processing: require("../images/heros/processing-hero-100.jpg"),
-    certification: require("../images/heros/certificate-hero-100.jpg"),
-};
 
 const Container = styled.div`
 position: relative;
@@ -49,9 +40,69 @@ transform: translate(-50%, -50%);
 `
 
 const Hero = ({tagline, path}) => {
+    const [imgSize, changeImgSize] = useState({
+        tablet: null,
+        mobile: null
+    });
+
+    // Performs new check on window size every time the window size is changed
+    useEffect(() => {
+        window.addEventListener('resize', handleWindowChange);
+        return _ => {
+            window.removeEventListener('resize', handleWindowChange);
+        }
+    })
+
+    // Run once after component mounts to determine current window size
+    useEffect(() => {
+        handleWindowChange();
+    }, [])
+
+    // Checks if the current window size is tablet or mobile
+    const handleWindowChange = () => {
+        changeImgSize({
+            ...imgSize,
+            tablet: (typeof window !== 'undefined') ? window.matchMedia("(max-width: 768px)").matches : null,
+            mobile: (typeof window !== 'undefined') ? window.matchMedia("(max-width: 414px)").matches : null
+        });
+    }
+
+    const desktopImages = {
+        home: require("../images/heros/homepage-hero-1920.jpg"),
+        about: require("../images/heros/About-moringa-hero-100.jpg"),
+        cultivation: require("../images/heros/Cultivation-hero-100.jpg"),
+        transportation: require("../images/heros/transportation-hero-100.jpg"),
+        processing: require("../images/heros/processing-hero-100.jpg"),
+        certification: require("../images/heros/certificate-hero-100.jpg"),
+    };
+
+    const tabletImages = {
+        home: require("../images/heros/homepage-hero-768.jpg"),
+        about: require("../images/heros/About-moringa-hero-100.jpg"),
+        cultivation: require("../images/heros/Cultivation-hero-100.jpg"),
+        transportation: require("../images/heros/transportation-hero-100.jpg"),
+        processing: require("../images/heros/processing-hero-100.jpg"),
+        certification: require("../images/heros/certificate-hero-100.jpg"),
+    };
+
+    const mobileImages = {
+        home: require("../images/heros/homepage-hero-414.jpg"),
+        about: require("../images/heros/About-moringa-hero-100.jpg"),
+        cultivation: require("../images/heros/Cultivation-hero-100.jpg"),
+        transportation: require("../images/heros/transportation-hero-100.jpg"),
+        processing: require("../images/heros/processing-hero-100.jpg"),
+        certification: require("../images/heros/certificate-hero-100.jpg"),
+    };
+
     return (
         <Container style={ (path === "cultivation" || path === "transportation" || path === "processing") ? {marginTop: "63px"} : {}}>
-            <div className="hero-img" style={{ backgroundImage: `url(${images[path]})` }}></div>
+            <div className="hero-img" style={{ backgroundImage: `url(${
+                imgSize.mobile ? 
+                mobileImages[path] :
+                imgSize.tablet ? 
+                tabletImages[path] :
+                desktopImages[path]
+                })` }}></div>
             <Tagline><span>{tagline}</span></Tagline>
         </Container>
 );
